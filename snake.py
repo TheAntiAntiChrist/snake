e7ad552b4b9347f8d7c228cefb7cf13d3581d967
Oscar <36861657+TheAntiAntiChrist@users.noreply.github.com>
2019-06-29 17:14:07 +0100

    Updated snake.py
    Updated code to prevent the snake from colliding w/ itself by pressing the arrow keys too quickly.
    Also added a sound effect whenever a "tic-tac" is eaten.

--- snake.py
+++ snake.py
@@ -1,12 +1,15 @@
 import pygame
 from random import randint
 
+pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
 screen = pygame.display.set_mode([600,600])
 pygame.display.set_caption("Snake")
 
 #snakeIcon = pygame.image.load("snake.png") #Code only useful if snake.png is present
 #pygame.display.set_icon(snakeIcon)
+
+#pointSound = pygame.mixer.Sound("ticTac.ogg") #Code only useful if ticTac.ogg is present
 
 Done = False
 clock = pygame.time.Clock()
@@ -16,32 +19,38 @@ snkDir = 0 # 0 is north, 1 is east, 2 is south, 3 is west
 counter = 0
 length = 1
 isEaten = True
+alreadyPressed = False
 
 def OOB():
     if snk[-1][0] < 0:
-        snk[-1][0] = 600
-    elif snk[-1][0] > 600:
-        snk[-1][0] = 0
+        return True
+    elif snk[-1][0] >= 600:
+        return True
     if snk[-1][1] < 0:
-        snk[-1][1] = 600
-    elif snk[-1][1] > 600:
-        snk[-1][1] = 0
+        return True
+    elif snk[-1][1] >= 600:
+        return True
 
 while not Done:
+    alreadyPressed = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Done = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 Done = True
-            if event.key == pygame.K_UP and snkDir != 2:
+            if event.key == pygame.K_UP and snkDir != 2 and alreadyPressed == False:
                 snkDir = 0
-            elif event.key == pygame.K_DOWN and snkDir != 0:
+                alreadyPressed = True
+            if event.key == pygame.K_DOWN and snkDir != 0 and alreadyPressed == False:
                 snkDir = 2
-            if event.key == pygame.K_RIGHT and snkDir != 3:
+                alreadyPressed = True
+            if event.key == pygame.K_RIGHT and snkDir != 3 and alreadyPressed == False:
                 snkDir = 1
-            elif event.key == pygame.K_LEFT and snkDir != 1:
+                alreadyPressed = True
+            if event.key == pygame.K_LEFT and snkDir != 1 and alreadyPressed == False:
                 snkDir = 3
+                alreadyPressed = True
 
     screen.fill([50,50,50])
 
@@ -51,20 +60,28 @@ while not Done:
     pygame.draw.rect(screen,[150,150,150],[ticTac[0],ticTac[1],10,10],0)
     pygame.draw.rect(screen,[255,255,255],[ticTac[0]+2,ticTac[1]+2,6,6],0)
 
-    OOB()
     if snkDir == 0:
         snk.append([snk[-1][0],snk[-1][1]-10])
-    if snkDir == 1:
+    elif snkDir == 1:
         snk.append([snk[-1][0]+10,snk[-1][1]]) #0+10
-    if snkDir == 2:
+    elif snkDir == 2:
         snk.append([snk[-1][0],snk[-1][1]+10]) #1+10
-    if snkDir == 3:
+    elif snkDir == 3:
         snk.append([snk[-1][0]-10,snk[-1][1]]) #0-10
     if snk[-1] == ticTac:
         length += 1
+        #pointSound.play() #Code only useful if ticTac.ogg is present
         isEaten = True
     if len(snk) > length:
         snk.pop(0)
+    if OOB():
+        print("""
+        You Died...
+        """)
+        print("""
+        You Scored: """, length-1, """ points.
+        """)
+        Done = True
     for segment in snk:
         pygame.draw.rect(screen,[100,200,200],[segment[0],segment[1],10,10],0)
         pygame.draw.rect(screen,[150,255,255],[segment[0]+2,segment[1]+2,6,6],0)
@@ -73,7 +90,7 @@ while not Done:
             You Died...
             """)
             print("""
-            You Scored: """, length, """ points.
+            You Scored: """, length-1, """ points.
             """)
             Done = True
 
